ff6f43ddf6123d3dcf52c64b402734701d39f88b
Marynes Rocca <54243253+Marynes13@users.noreply.github.com>
2023-01-20 18:07:11 -0500

    Create Temperature.py

---
+++ Temperature.py
@@ -0,0 +1,10 @@
+def check_temp(temp):
+  if temp > 28:
+    return "Hot"
+  elif temp >= 18 and temp <= 28:
+    return "Warm"
+  else:
+    return "Cold"
+
+  temp = int(input("Enter temperature: "))
+  print(check_temp(temp))
